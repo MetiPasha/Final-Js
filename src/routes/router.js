cc08225eec2508a082212983f98bfea9eb480fb6
pasha <mtipasha2021@gmail.com>
2024-11-22 14:03:35 +0330

    put these tow things swiper2&swiper1

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,7 +3,8 @@ import login from "../pages/loginPage";
 import starter from "../pages/starter";
 import welcome from "../pages/welcome";
 import swiper1 from "../pages/swiper1";
-
+import swiper2 from "../pages/swiper2";
+import swiper3 from "../pages/swiper3";
 export const router = new Navigo("/");
 const changePage = (target) => {
   const root = document.getElementById("app");
@@ -20,4 +21,10 @@ router
   })
   .on("/swiper1", () => {
     changePage(swiper1);
+  })
+  .on("/swiper2", () => {
+    changePage(swiper2);
+  })
+  .on("/swiper3", () => {
+    changePage(swiper3);
   });
